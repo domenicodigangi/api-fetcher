59d722576b3cb2f71cc28c044a5682f8a1cc57fd
Domenico Di Gangi <digangidomenico@gmail.com>
2024-01-06 18:07:42 +0000

    wip on generalizing apit fetching to other apis

--- src/api_fetcher/__init__.py
+++ src/api_fetcher/__init__.py
@@ -8,7 +8,7 @@ import pandas as pd
 from api_fetcher.async_task_helper import AsyncTaskHelper
 from api_fetcher.cache.redis import RedisCache
 from api_fetcher.data_format.dataframe import FormattedDataType, PolarsDataFormatter
-from api_fetcher.settings import DomotzAPISettings
+from api_fetcher.settings import APISettings
 from pydantic import BaseModel
 
 logging.basicConfig(level=logging.INFO)
@@ -24,7 +24,7 @@ class CachedDataFormat(BaseModel):
 
 
 class DomotzAPIDataFetcher:
-    def __init__(self, api_settings: DomotzAPISettings, cache=None):
+    def __init__(self, api_settings: APISettings, standard_calls=dict, cache=None):
         self.task_helper = AsyncTaskHelper()
         self._api_settings = api_settings
         self._cache = cache or RedisCache(ttl=self._api_settings.cache_ttl)
@@ -34,6 +34,7 @@ class DomotzAPIDataFetcher:
             self._api_settings.days_history
         )
         self.data_formatter = PolarsDataFormatter()
+        self._standard_calls = standard_calls
 
     def get_key_prefix(self) -> str:
         return hashlib.sha256(self._api_settings.api_key.encode()).hexdigest()
@@ -46,36 +47,15 @@ class DomotzAPIDataFetcher:
 
     @property
     def standard_calls(self):
-        return {
-            "agents_list": {"path": "/agent", "params": {}},
-            "agent": {"path": "/agent/{agent_id}", "params": {}},
-            "agent_status_history": {
-                "path": "/agent/{agent_id}/history/network/event",
-                "params": {},
-            },
-            "list_devices": {
-                "path": "/agent/{agent_id}/device",
-                "params": {"show_hidden": True},
-            },
-            "list_device_variables": {
-                "path": "/agent/{agent_id}/device/variable",
-                "params": {"page_size": 1000, "has_history": "true"},
-            },
-            "device_inventory": {
-                "path": "/agent/{agent_id}/device/{device_id}/inventory",
-                "params": {},
-            },
-            "history_device_variable": {
-                "path": "/agent/{agent_id}/device/{device_id}/variable/{variable_id}/history",
-                "params": {"from": self._start_date_history},
-            },
-        }
+        return self._standard_calls
 
     async def get_iterator(self, item: str, list_path_params: List[Dict]):
         for path_params in list_path_params:
             yield await self.get(item, path_params=path_params)
 
-    async def get(self, item: str, path_params: Optional[Dict] = None):
+    async def get(
+        self, item: str, path_params: Optional[Dict] = None
+    ) -> CachedDataFormat:
         if item in self.standard_calls:
             if path_params is None:
                 url = self.standard_calls[item]["path"]
@@ -109,7 +89,7 @@ class DomotzAPIDataFetcher:
         return self.data_formatter.format_response(resource_path, response)
 
     async def _api_get(self, resource_path: str, **kwargs):
-        url = f"{self._api_settings.public_api_url}{resource_path}"
+        url = f"{self._api_settings.base_url}{resource_path}"
         logger.info("Get from url: %s", url)
         async with httpx.AsyncClient() as client:
             if "params" in kwargs:
@@ -129,77 +109,6 @@ class DomotzAPIDataFetcher:
     def get_cache_key(self, path: str, params: Dict | None = None) -> str:
         return f"{self.key_prefix}{path}{params}"
 
-    async def get_all_variables_from_agent(
-        self, agent_id: int
-    ) -> Tuple[FormattedDataType, dict[str, dict]]:
-        async def get_history_device_variable(
-            agent_id: int, device_id: int, variable_id: int
-        ) -> CachedDataFormat:
-            return await self.get(
-                "history_device_variable",
-                path_params={
-                    "agent_id": agent_id,
-                    "device_id": device_id,
-                    "variable_id": variable_id,
-                },
-            )
-
-        variables = await self.get("list_device_variables", {"agent_id": agent_id})
-        variables_history = {}
-        df_variables = variables.data
-        df_variables = df_variables.loc[df_variables["has_history"], :]
-        df_variables["history_hash"] = None
-        df_variables["cache_key"] = None
-
-        task_res = await self.task_helper.define_and_gather_task(
-            get_history_device_variable,
-            [
-                (agent_id, row["device_id"], row["id"])
-                for ind, row in df_variables.iterrows()
-            ],
-            args_to_ret_inds=[2],
-        )
-
-        for safe_res in task_res:
-            (
-                success,
-                res,
-                args_retuned,
-                kwargs_retuned,
-            ) = safe_res
-            variable_id = args_retuned[0]
-            var_ind = df_variables["id"] == variable_id
-            if success and res is not None:
-                df_history = res.data
-                variables_history[res.cache_key] = {
-                    "hist": df_history,
-                }
-                df_variables.loc[var_ind, "history_hash"] = hash(df_history.to_json())
-                df_variables.loc[var_ind, "cache_key"] = res.cache_key
-                df_variables.loc[var_ind, "history_len"] = df_history.shape[0]
-            else:
-                df_variables.loc[var_ind, "has_history"] = False
-
-        df_variables = (
-            df_variables.loc[df_variables["has_history"], :]
-            .groupby(by=["history_hash", "path"])
-            .agg(
-                cache_key=("cache_key", "first"),
-                device_id=("device_id", "first"),
-                id=("id", "first"),
-                replication_count=("id", "count"),
-                metric=("metric", "first"),
-                unit=("unit", "first"),
-            )
-            .reset_index()
-        )
-        variables_history = {
-            k: v
-            for k, v in variables_history.items()
-            if k in df_variables["cache_key"].tolist()
-        }
-        return df_variables, variables_history
-
 
 # if __name__ == "__main__":
 #     from api_fetcher.settings import BASE_URLS
@@ -207,7 +116,7 @@ class DomotzAPIDataFetcher:
 
 #     env = dotenv_values("/workspaces/anomaly-detection-iot/.env")
 #     api_fetcher = DomotzAPIDataFetcher(
-#         DomotzAPISettings(api_key=env["API_KEY_EU"], base_url=BASE_URLS["EU"])
+#         APISettings(api_key=env["API_KEY_EU"], base_url=BASE_URLS["EU"])
 #     )
 #     api_fetcher.clear_cache()
 #     agents = await api_fetcher.get("agents_list")
--- src/api_fetcher/settings.py
+++ src/api_fetcher/settings.py
@@ -2,15 +2,33 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 BASE_URLS = {
-    "EU": "https://api-eu-west-1-cell-1.domotz.com",
-    "US": "https://api-us-east-1-cell-1.domotz.com",
+    "EU": "https://api-eu-west-1-cell-1.domotz.com/public-api/v1",
+    "US": "https://api-us-east-1-cell-1.domotz.com/public-api/v1",
 }
 
 
-class DomotzAPISettings(BaseSettings):
+class APISettings(BaseSettings):
     api_key: str | None = Field(None, env="API_KEY")
     base_url: str = Field(default=BASE_URLS["US"], env="BASE_URL")
     cache_ttl: int = Field(default=3600, env="CACHE_TTL")
+    days_history: int = Field(default=14, env="DAYS_HISTORY")
+
+    class Config:
+        env_file = ".env"
+
+    @property
+    def headers(self):
+        return None
+
+    def usable_api_key(self) -> bool:
+        if self.api_key not in ["", None]:
+            if len(self.api_key) >= 32:
+                return True
+        else:
+            return False
+
+
+class DomotzAPISettings(APISettings):
     days_history: int = Field(default=14, env="DAYS_HISTORY")
 
     class Config:
@@ -19,14 +37,3 @@ class DomotzAPISettings(BaseSettings):
     @property
     def headers(self):
         return {"Accept": "application/json", "X-Api-Key": self.api_key}
-
-    @property
-    def public_api_url(self):
-        return f"{self.base_url}/public-api/v1"
-
-    def usable_api_key(self) -> bool:
-        if self.api_key not in ["", None]:
-            if len(self.api_key) >= 32:
-                return True
-        else:
-            return False
